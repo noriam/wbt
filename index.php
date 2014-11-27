233938a7cbb46ac99e16638f48acc010db0384a8
valentin <pasque_v@etna-alternance.net>
2014-11-27 17:16:37 +0100

    change index.html to index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="style.css" rel="stylesheet">
+    <div class="header">Welcome to the Wesnoth balancing tool page !
+    </div>
+</head>
+<body>
+<div class="unit_to_compare">
+    <?php include("chara_page.php"); ?>
+</div>
+<div class="unit_of_reference">
+</div>
+<div class="footer">
+</div>
+</body>
+</html>

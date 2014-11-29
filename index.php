1d17a33892b4141458f98c7d3d368f5ecfd0581e
valentin <pasque_v@etna-alternance.net>
2014-11-29 02:56:02 +0100

    Add new Font, icon image and header + link to co-labs project

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <!DOCTYPE html>
+
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="author" content="Romain Chicoisne, Gautier Melot, Duncan Naves & Valentin Pasqué">
+    <link rel="shortcut icon" href="Images/icon.png">
+    <title>WBT</title>
     <link href="style.css" rel="stylesheet">
-    <div class="header">Welcome to the Wesnoth balancing tool page !
-    </div>
 </head>
 <body>
+<div class="header"><div class="main_title">Wesnoth Balancing Tool</div>
+    <br>
+    By Romain Chicoisne, Gautier Melot, Duncan Naves & Valentin Pasqué
+    <br>
+    <a href="https://co-labs.etna.io/projets/WesnothBalancingTool" target="_blank">Co-labs Page</a>
+</div>
 <?php include("form.php") ?>
 </body>
 </html>
